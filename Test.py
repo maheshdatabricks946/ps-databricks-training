6e75d3a699030c52ca2f02a4fd34758b3f781d9c
maheshdatabricks946 <125427980+maheshdatabricks946@users.noreply.github.com>
2023-02-15 15:05:19 +0530

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,16 @@
+import pytest
+import matplotlib.pyplot as plot
+import numpy as np
+
+def sine_wave():
+    time = np.arange(-8,8, 0.1)
+    # amplitude = np.sin(time)
+    # plot.plot(time, amplitude)
+    # plot.show()
+
+    amplitude = np.cos(time)
+    plot.plot(time, amplitude)
+    plot.show()
+
+if __name__ == "__main__":
+    sine_wave()
