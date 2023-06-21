a1e4c214c197b2416b7a4833bd31f5bde904eefa
maheshdatabricks946 <maheshdatabricks946>
2023-06-21 14:26:39 +0000

    added new files

---
+++ db_test4_2023_06_21 19_50_42.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### 1.5B - Views Session-2
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SHOW TABLES;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SHOW TABLES IN global_temp;
+
+# COMMAND ----------
+
+
